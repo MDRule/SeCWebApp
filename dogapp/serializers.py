94c669eca5f41cc9a0e7f86d18c6241c824760a9
Md Monirul Islam <mdmonirulislam@unomaha.edu@MacBook-Air.local>
2024-10-14 14:36:46 -0500

    requirements updated

--- dogapp/serializers.py
+++ dogapp/serializers.py
@@ -1,7 +1,14 @@
 from rest_framework import serializers
-from .models import Dog
+from .models import Dog, Breed
+
+class BreedSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Breed
+        fields = ['id', 'name', 'size', 'friendliness', 'trainability', 'sheddingamount', 'exerciseneeds']
 
 class DogSerializer(serializers.ModelSerializer):
+    breed = BreedSerializer()  # Nesting the BreedSerializer to show full breed details in the dog response
+
     class Meta:
         model = Dog
-        fields = ['id', 'name', 'age', 'breed']
+        fields = ['id', 'name', 'age', 'breed', 'gender', 'color', 'favoritefood', 'favoritetoy']
--- webservices/urls.py
+++ webservices/urls.py
@@ -1,52 +1,50 @@
-"""
-URL configuration for webservices project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
-
-from django.urls import path
-from dogapp.views import dog_service
-
-from dogapp.views import rest_get_dog
-from django.contrib.auth import views as auth_views
-
-
-
-from graphene_django.views import GraphQLView
 from django.views.decorators.csrf import csrf_exempt
-from dogapp.schema import schema
 
-from dogapp.views import DogDetailAPIView
+# Import views from the dogapp for REST and SOAP endpoints
+from dogapp.views import (
+    dog_service,
+    rest_get_dog,
+    DogList,
+    DogDetail,
+    BreedList,
+    BreedDetail
+)
 
-
+# JWT views
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
 
+# GraphQL view
+from graphene_django.views import GraphQLView
+from dogapp.schema import schema
 
+# URL patterns
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # SOAP Service for Dogs
     path('soap/dogservice/', dog_service),
-    path('rest/dog/<int:dog_id>/', rest_get_dog, name='rest_get_dog'),
+
+    # REST API for Dogs
+    path('api/dogs/', DogList.as_view(), name='dog-list'),
+    path('api/dogs/<int:pk>/', DogDetail.as_view(), name='dog-detail'),
+
+    # REST API for Breeds
+    path('api/breeds/', BreedList.as_view(), name='breed-list'),
+    path('api/breeds/<int:pk>/', BreedDetail.as_view(), name='breed-detail'),
+
+    # GraphQL endpoint
     path('graphql/', csrf_exempt(GraphQLView.as_view(graphiql=True, schema=schema))),
-    path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
-    path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
+
+    # JWT Authentication
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('rest/dog/<int:pk>/', DogDetailAPIView.as_view(), name='rest_get_dog'),
 
+    # Account login/logout
+    path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
+    path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
